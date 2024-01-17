6a1513641701ee640bcb06148206a22e04be5112
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-01-16 22:51:47 -0500

    Add files via upload

---
+++ D/011.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejemplo {
+	//Esta es una clase propia con sus atributos y métodos (encapsulación)
+	class MiClase {
+		//Un constructor
+		public MiClase(int Numero, char Letra, string Cadena, double Valor) {
+			this.Numero = Numero; //Se asigna así this.atributo = valor parámetro
+			this.Letra = Letra;
+			this.Cadena = Cadena;
+			this.Valor = Valor;
+		}
+
+		//Otra forma de definir atributos con los getters y setters
+		public int Numero { get; set; }
+		public char Letra { get; set; }
+		public string Cadena { get; set; }
+		public double Valor { get; set; }
+	}
+	
+	//Inicia la aplicación aquí
+	class Program {
+		static void Main() {
+			//Instancia o crea un objeto de MiClase llamando el constructor
+			MiClase Mascotas = new MiClase(2016, 'T', "Tammy", 12.17);
+
+			//Se imprimen los valores de ambas variables
+			Console.WriteLine("Letra en Mascotas es: " + Mascotas.Letra.ToString());
+			Console.WriteLine("Valor en Mascotas es: " + Mascotas.Valor.ToString());
+		}
+	}
+}
