a6c468b841b81cdcf8ce45f56ce0e94320f8731e
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-02-04 07:20:39 -0500

    Add files via upload

---
+++ I/003.cs
@@ -0,0 +1,18 @@
+namespace Ejemplo {
+	internal class Program {
+		static void Main() {
+			//Generador Blum Blum Shub
+			long X0, M;
+
+			//Valores de inicio. Se los da el usuario.
+			X0 = 3;
+			M = 11*19;
+
+			for (int contador = 1; contador <= 100; contador++) {
+				X0 = (X0*X0) % M;
+				double r = (double) X0 / M;
+				Console.WriteLine("Número pseudo-aleatorio: " + X0 + "  r: " + r);
+			} 
+		}
+	}
+}
