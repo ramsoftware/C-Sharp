c870a8fd0de09d31d4554679b6a48e59e267acd9
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-04-01 17:08:25 -0500

    Versión 3 del libro de C#

---
+++ I/006.cs
@@ -0,0 +1,27 @@
+namespace Ejemplo {
+	internal class Program {
+		static void Main(string[] args) {
+			/* Distribución Triangular. Generar una variable aleatoria con valor mínimo A,
+				 valor más probable B y valor máximo C
+				 Si r < (B-A)/(C-A)
+					Variable = A + raizcuadrada(r*(C-A)*(B-A))
+				 de lo contrario
+					Variable = C - raizcuadrada((1-r)*(C-A)*(C-B))
+				 */
+			Random Azar = new Random();
+
+			double A = 150;
+			double B = 190;
+			double C = 230;
+			double variable = 0;
+			for (int cont = 1; cont <= 100; cont++) {
+				double r = Azar.NextDouble();
+				if (r < (B - A) / (C - A))
+					variable = A + Math.Sqrt(r * (C - A) * (B - A));
+				else
+					variable = C - Math.Sqrt((1 - r) * (C - A) * (C - B));
+				Console.Write(variable + "; ");
+			}
+		}
+	}
+}
