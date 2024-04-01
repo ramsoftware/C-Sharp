dffe379933457fab747574763579fd17e8e4ceed
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-04-01 17:06:42 -0500

    Versión 3 del libro de C#

---
+++ E/003.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace Ejemplo {
+	class Program {
+		public static void Main() {
+			//Declara la lista que almacenará cadenas
+			ArrayList Ejemplo = new ArrayList();
+
+			//Adiciona elementos a la lista
+			Ejemplo.Add("Ballena");
+			Ejemplo.Add("Tortuga marina");
+			Ejemplo.Add("Tiburón");
+			Ejemplo.Add("Estrella de mar");
+			Ejemplo.Add("Hipocampo");
+			Ejemplo.Add("Serpiente marina");
+			Ejemplo.Add("Delfín");
+			Ejemplo.Add("Pulpo");
+			Ejemplo.Add("Caballito de mar");
+			Ejemplo.Add("Coral");
+			Ejemplo.Add("Pingüinos");
+			Ejemplo.Add("Calamar");
+			Ejemplo.Add("Gaviota");
+			Ejemplo.Add("Foca");
+			Ejemplo.Add("Manaties");
+
+			//Imprime valores
+			foreach (Object objeto in Ejemplo) Console.Write("{0}{1}", ";", objeto);
+			Console.WriteLine("\r\n");
+
+			//Cambia una cadena en la lista
+			Ejemplo[3] = "CACATÚA";
+
+			//Imprime valores
+			foreach (Object objeto in Ejemplo) Console.Write("{0}{1}", ";", objeto);
+		}
+	}
+}
---
+++ E/012.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Ejemplo {
+	class Program {
+		public static void Main() {
+			//Declara tres ArrayList
+			ArrayList ListaA = new ArrayList();
+			ArrayList ListaB = new ArrayList();
+			ArrayList ListaC = new ArrayList();
+
+			//Adiciona elementos a esos ArrayList
+			ListaA.Add("A");
+			ListaA.Add("B");
+			ListaA.Add("C");
+			ListaB.Add("7");
+			ListaB.Add("8");
+			ListaB.Add("9");
+			ListaC.Add("qw");
+			ListaC.Add("er");
+			ListaC.Add("ty");
+
+			//Inserta los dos primeros ArrayList en el tercero
+			int posicionInserta = 1;
+			ListaC.InsertRange(posicionInserta, ListaA);
+
+			posicionInserta = 5;
+			ListaC.InsertRange(posicionInserta, ListaB);
+
+			//Imprime el ArrayList
+			for (int cont = 0; cont < ListaC.Count; cont++) Console.Write(ListaC[cont] + "; ");
+			Console.WriteLine("\r\n");
+
+			//Modifica ListaA y se chequea si eso afectó a ListaC 
+			ListaA[0] = "CACATÚA";
+			for (int cont = 0; cont < ListaC.Count; cont++) Console.Write(ListaC[cont] + "; ");
+		}
+	}
+}
