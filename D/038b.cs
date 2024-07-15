2c7f927cde44b4257b5ca26d4cab0ce62a8e818b
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-07-15 00:09:42 -0500

    Delete D directory

--- D/038b.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ejemplo {
-	partial class MiClase {
-		public int ValorA { get; set; }
-		public double ValorB { get; set; }
-		public char ValorC { get; set; }
-		public string ValorD { get; set; }
-	}
-}
